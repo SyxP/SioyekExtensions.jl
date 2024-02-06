e3b41e641b78d4cd3623fef5b4f9c01380b3040c
Syx Pek <20557013+SyxP@users.noreply.github.com>
2024-02-06 08:06:23 +0800

    Initial work for ChatGPT integration

--- src/SioyekTranslateChatGPT.jl
+++ src/SioyekTranslateChatGPT.jl
@@ -1,3 +1,26 @@
 module SioyekTranslateChatGPT
+    # Precompilation: https://discourse.julialang.org/t/how-do-you-make-standalone-programs-in-julia/105218/17
+
+    using PrecompileTools
+
+    const DRIVER_FILE = @__FILE__
+    function __init__()
+        if abspath(PROGRAM_FILE) == DRIVER_FILE
+            main(ARGS)
+        end
+    end
+
+    function main(ARGS ::Vector{String})
+        sioyekPath = ARGS[1]
+        toRun = Cmd([sioyekPath, "--execute-command", "set_status_string", "--execute-command-data", "a\nb\nc"])
+        run(toRun)
+    end
+    
+    @setup_workload begin
+        precompile_args = ["randomPath"]
+        @compile_workload begin
+            main(precompile_args)
+        end
+    end
 
 end
